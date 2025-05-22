45ec526ca805575c81015c0e478ed18fe959b0c4
SurpassHR <hu.rui0530@gmail.com>
2025-05-22 21:31:45 +0800

    feat: UI 配置界面存档目录选择

--- ui/components/pages/configPage.py
+++ ui/components/pages/configPage.py
@@ -1,21 +1,16 @@
 import sys
-import uuid
-from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QFrame, QBoxLayout
+from PyQt5.QtWidgets import QFrame, QVBoxLayout, QFileDialog
 from pathlib import Path
 
-from qfluentwidgets import PrimaryPushButton
-from qfluentwidgets.common.icon import FluentIcon
-
-from ui.components.utils.eventManager import EventEnum
+from qfluentwidgets import FluentIcon
 
 rootPath = str(Path(__file__).resolve().parent.parent.parent.parent)
 sys.path.append(rootPath)
 
-from core.getGitInfo import CommitObj
+from core.tools.utils.configLoader import getConfig, setConfig
+
 from ui.components.utils.uiFunctionBase import UIFunctionBase
-from ui.components.widgets.layouts.infiniteCanvasView import InfiniteCanvasView
-from ui.components.widgets.layouts.gridScene import SmartGridScene
-from ui.publicDefs.styleDefs import NODE_VERTICAL_SPACING
+from ui.components.widgets.frames.pushButtonCard import PushButtonCard
 
 class ConfigPage(QFrame, UIFunctionBase):
     def __init__(self, text: str, window) -> None:
@@ -23,4 +18,40 @@ class ConfigPage(QFrame, UIFunctionBase):
 
         self.setObjectName(text.replace(" ", "-"))
 
+        # 主容器
+        container = QVBoxLayout()
+        container.setSpacing(8)
+
+        container.addWidget(self.createSelectRepoCard())
+        container.addStretch(1)
+
         self.window = window
+
+        self.setLayout(container)
+
+    def createSelectRepoCard(self) -> PushButtonCard:
+        def selectRepoCardInit(widget: PushButtonCard):
+            selectedRepo = "当前管理存档目录: " + f"{getConfig("repo")}"
+            widget.setDescription(f"{selectedRepo}")
+            widget.setText("选择文件夹")
+            widget.setIcon(FluentIcon.FOLDER_ADD)
+
+        def selectRepoCardClicked(widget: PushButtonCard):
+            # 选择文件夹
+            path = QFileDialog.getExistingDirectory(
+                parent=None,
+                caption="选择目录",
+            )
+            path = path.strip()
+            if path is None or path == "":
+                return
+            setConfig("repo", path)
+            selectedRepo = "当前管理存档目录: " + f"{getConfig("repo")}"
+            widget.setDescription(f"{selectedRepo}")
+
+        return PushButtonCard(
+            title="选择存档目录",
+            description="选择存档目录",
+            init=selectRepoCardInit,
+            clicked=selectRepoCardClicked,
+        )
